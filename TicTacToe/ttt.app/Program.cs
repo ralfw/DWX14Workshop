3a6543bd667289caaab3697f25003bda86e26f74
Ralf Westphal <info@ralfw.de>
2014-07-17 19:36:26 +0200

    build/bind/run in Main() eingezogen

--- TicTacToe/ttt.app/Program.cs
+++ TicTacToe/ttt.app/Program.cs
@@ -19,20 +19,26 @@ namespace ttt.app
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
+            // build
             var es = new EventStore("eventstore.db");
             var ui = new UI();
+
             var sb = new Spielbrett(es.History);
-            es.OnAppended += sb.Update;
+
             var sp = new Spiel(es);
             var schiri = new Schiedsrichter(es);
             var map = new Mapper(sb);
             var app = new App(sp, schiri, map);
+
+            // bind
+            es.OnAppended += sb.Update;
 
             app.Spielstand_aktualisiert += ui.Spielstand_anzeigen;
 
             ui.Neues_Spiel_gewünscht += app.Neues_Spiel;
             ui.Spielstein_gesetzt += app.Zug_ausführen;
 
+            // run
             app.Starten();
 
             Application.Run(ui);
